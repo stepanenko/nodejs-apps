68d7a8d40ca1b91c25d56a3b54a306e227da2331
stepanenko <serhiy.stepanenko@aiesec.net>
2020-02-22 11:59:04 +0200

    adding task implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,61 @@
 
 import React, { Component } from 'react';
 import './App.css';
+import { Task } from './models/task';
+import { NewTaskForm } from './components/NewTaskForm';
 
-export default class App extends Component {
+
+interface State {
+  newTask: Task;
+  tasks: Task[];
+}
+
+export default class App extends Component<{}, State> {
+  state = {
+    newTask: {
+      id: 1,
+      name: ''
+    },
+    tasks: []
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <p>Hello REACT</p>
+          <p>Hello REACT TS</p>
+          <NewTaskForm
+            task={ this.state.newTask }
+            onAdd={ this.addTask }
+            onChange={ this.handleTaskChange }
+          />
         </header>
       </div>
     );
   }
+
+  private addTask = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    this.setState(previousState => ({
+      newTask: {
+        id: previousState.newTask.id + 1,
+        name: ''
+      },
+      tasks: [...previousState.tasks, previousState.newTask]
+    }));
+
+    console.log(this.state.newTask);
+    console.log(this.state.tasks);
+  };
+
+  private handleTaskChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      newTask: {
+        ...this.state.newTask,
+        name: event.target.value
+      }
+    });
+  };
+
 }
